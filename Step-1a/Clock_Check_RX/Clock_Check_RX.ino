fda7ec5b27c1d81cf4648730f34c29446cd979f8
svdeepak99 <42609308+svdeepak99@users.noreply.github.com>
2021-02-22 02:26:53 +0530

    Wired Calibration Arduino Oscillators

---
+++ Step-1a/Clock_Check_RX/Clock_Check_RX.ino
@@ -0,0 +1,38 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+//#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+int n=100;
+unsigned int t;
+
+int main()
+{
+  Serial.begin(1000000);
+  //Timer 1 CTC code
+  ICR1=39953;
+  TCCR1B|=(1<<WGM12)|(1<<WGM13)|(1<<CS11); //CTC with ICR1 as TOP , Prescaler 8
+  //INT1 code
+  sbi(EIMSK,INT1);
+  EICRA|=(1<<ISC10)|(1<<ISC11); //Rising Edge
+  sei();
+  while(1);
+  return 0;
+}
+
+ISR(INT1_vect)
+{
+  if(n==100){
+    TCNT1=0;
+    t=TCNT1;
+    n=0;
+  }
+  else
+    t=TCNT1;
+  Serial.print(n);Serial.print(") ");Serial.println(t);
+  n++;
+}
+
---
+++ Step-1a/Clock_Check_TX/Clock_Check_TX.ino
@@ -0,0 +1,20 @@
+#include<avr/io.h>
+//#include<avr/interrupt.h>
+//#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+int main()
+{
+  //Timer 1 PWM code
+  sbi(DDRB,1);
+  ICR1=40000;
+  OCR1A=20000;
+  TCCR1A|=(1<<WGM11)|(1<<COM1A1); //set at BOTTOM, clear on Compare Match
+  TCCR1B|=(1<<WGM12)|(1<<WGM13)|(1<<CS11);  //Fast PWM with ICR1 as TOP , Prescaler 8
+  while(1);
+  return 0;
+}
+
