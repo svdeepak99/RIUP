385e80ab5ff889a17e113ac32c2571c3f5538086
svdeepak99 <42609308+svdeepak99@users.noreply.github.com>
2021-02-22 03:15:48 +0530

    Measuring RF Signal Propagation Delay

---
+++ Step-2/RF_Response1.1_TX_beta_nocom/RF_Response1.1_TX_beta_nocom.ino
@@ -0,0 +1,54 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+int n;
+
+void rf_pwm_init()
+{
+  sbi(DDRD,5);
+  TCCR0A|=(1<<WGM00)|(1<<COM0B1); //Set on down counting, Clear on up counting      
+  sbi(TCCR0B,WGM02);              //Phase correct PWM with OCR0A as top
+  OCR0A=100;
+  OCR0B=50;
+  TCNT0=100;
+  sei();
+  sbi(TCCR0B,CS01);       //prescaler 8
+}
+inline void on_pwm()
+{
+//  sbi(TCCR0B,CS01);
+  OCR0A=170;
+  OCR0B=85;
+  TCNT0=0;
+  TIMSK0|=1;      //Enable TOV (at 0) interrupt
+}
+inline void off_pwm()
+{
+  cbi(TIMSK0,0);
+  OCR0A=100;
+  OCR0B=50;
+  n=0;
+}
+int main()
+{
+  rf_pwm_init();
+  while(1)
+  {
+    on_pwm();
+    _delay_ms(5000);
+  }
+  return 0;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+  n++;
+  if(n==70)
+    off_pwm();
+}
+
---
+++ Step-2/RF_Response1.2_RX_beta_nocom/RF_Response1.2_RX_beta_nocom.ino
@@ -0,0 +1,56 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+//#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+unsigned long int n=0,i=0,t;
+
+inline void usart_send(unsigned int ch )
+{
+  while (UCSR0A != (UCSR0A | (1 << UDRE0))); //waiting for UDRE to become high - which means data is ready to be written
+  UDR0 = ch;
+}
+
+void rf_receive_init()
+{
+  Serial.begin(2000000);
+  sbi(EIMSK,INT0);
+  EICRA|=(1<<ISC00)|(1<<ISC01);     //Trigger at rise
+  sei();
+  sbi(TCCR1B,CS11);       //prescaler 8
+}
+
+int main()
+{//This code has best accuracy with antennas +/- 1 us max when measured at close range
+ //Best results are obtained when Transmitter has an antenna and receiver does not have an antenna
+  rf_receive_init();
+  while(1);
+  return 0;
+}
+
+ISR(INT0_vect)
+{
+  t=TCNT1;
+  if(t>270&&t<430){
+  TCNT1=0;
+  n+=t;
+  if(i==4)
+    n=0;
+  i++;
+  if(i==69){
+    n=n>>6;
+    Serial.println(n);
+    n=0;
+    i=0;    
+  }
+  }
+  else{
+    TCNT1=0;
+    i=0;
+    n=0;
+  }
+}
+
