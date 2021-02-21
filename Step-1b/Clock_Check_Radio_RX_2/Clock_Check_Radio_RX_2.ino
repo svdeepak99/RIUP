3d1d82dba167d7e0c0ad2e9a0bd000dccdd500b5
svdeepak99 <42609308+svdeepak99@users.noreply.github.com>
2021-02-22 02:43:51 +0530

    Wireless Calibration of Arduino Oscillators

---
+++ Step-1b/Clock_Check_Radio_RX_2/Clock_Check_Radio_RX_2.ino
@@ -0,0 +1,91 @@
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+#define TOP 39951
+
+int n1=25,t2,i,flag;
+unsigned int t1;
+int n=0;
+
+void rf_pwm_init()
+{
+  sbi(DDRD,5);
+  TCCR0A|=(1<<WGM00)|(1<<COM0B1); //Set on down counting, Clear on up counting      
+  sbi(TCCR0B,WGM02);              //Phase correct PWM with OCR0A as top
+  OCR0A=100;
+  OCR0B=50;
+  TCNT0=100;
+//  sei();
+  sbi(TCCR0B,CS01);       //prescaler 8
+}
+inline void on_pwm()
+{
+//  sbi(TCCR0B,CS01);
+  OCR0A=170;
+  OCR0B=85;
+  TCNT0=0;
+  TIMSK0|=1;      //Enable TOV (at 0) interrupt
+}
+inline void off_pwm()
+{
+  cbi(TIMSK0,0);
+  OCR0A=100;
+  OCR0B=50;
+  n=0;
+}
+
+int main()
+{
+  Serial.begin(2000000);
+  //Timer 1 CTC code
+  ICR1=TOP;
+  TCCR1B|=(1<<WGM12)|(1<<WGM13)|(1<<CS11); //CTC with ICR1 as TOP , Prescaler 8
+  TIMSK1|=(1<<1); //OCIEA Interrupt Enable
+  OCR1A=TOP-100;
+  //INT1 code
+  sbi(EIMSK,INT1);
+  EICRA|=(1<<ISC10)|(1<<ISC11); //Rising Edge
+  rf_pwm_init();
+  sei();
+  while(1){
+    while(flag)
+      _delay_ms(3);
+    Serial.print(n1);Serial.print(") ");Serial.println(t2);
+    flag=1;
+  }
+  return 0;
+}
+
+ISR(INT1_vect)
+{
+  if(n1==25){
+    on_pwm();
+    n1=0;
+  }
+  t1=TCNT1;
+  t2=(t1>30000)?t1-TOP:t1;
+  if(flag)
+    flag=0;
+  n1++;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+  if(n==6){
+    TCNT1=-82;
+    off_pwm();
+  }
+  else
+    n++;
+}
+
+ISR(TIMER1_COMPA_vect)
+{//The exact required TOP is 39950.5 .So is this simple technique :-)
+  ICR1=(ICR1==39950)?39951:39950;
+}
+
---
+++ Step-1b/Clock_Check_Radio_TX_2/Clock_Check_Radio_TX_2.ino
@@ -0,0 +1,58 @@
+#include<avr/io.h>
+//#include<avr/interrupt.h>
+//#include<util/delay.h>
+
+#define sbi(X,Y) X|=(1<<Y)
+#define cbi(X,Y) X&=~(1<<Y)
+#define tog(X,Y) X^=(1<<Y)
+
+long int i=0,t;
+uint16_t TCNT0H=0;
+
+#define TCNT0T (TCNT0H<<8)|TCNT0;
+
+void rf_receive_init()
+{
+  Serial.begin(2000000);
+  sbi(EIMSK,INT0);
+  EICRA|=(1<<ISC00)|(1<<ISC01);     //Trigger at rise
+  TIMSK0|=1;  //TOV interrut enable
+  sei();
+  sbi(TCCR0B,CS01);       //prescaler 8
+}
+
+int main()
+{
+  rf_receive_init();
+  //Timer 1 PWM code
+  sbi(DDRB,1);
+  ICR1=40000;
+  OCR1A=20000;
+  TCCR1A|=(1<<WGM11)|(1<<COM1A1); //set at BOTTOM, clear on Compare Match
+  TCCR1B|=(1<<WGM12)|(1<<WGM13)|(1<<CS11);  //Fast PWM with ICR1 as TOP , Prescaler 8
+  while(1);
+  return 0;
+}
+
+ISR(INT0_vect)
+{
+  if(i==5)
+      TCNT1=0;
+  t=TCNT0T;
+  TCNT0=0;
+  TCNT0H=0;
+  if(t>270&&t<430){
+    if(i==5)
+      i=0;
+    else
+      i++;
+  }
+  else
+    i=0;
+}
+
+ISR(TIMER0_OVF_vect)
+{
+  TCNT0H++;
+}
+
